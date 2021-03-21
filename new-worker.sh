c44f8d5a57a52e676ac6b8b75e8cb3f998b25461
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-03-21 16:19:55 +1100

    Rename worker-template to template-worker
    I just want autocomplete...

--- new-worker.sh
+++ new-worker.sh
@@ -5,8 +5,7 @@
 set -eu
 
 mkdir -p "workers/$1"
-
-cp worker-template/index.js "workers/$1"
-cp worker-template/build.sh "workers/$1"
-sed "s/WORKER_NAME/$1/" < worker-template/package.json > "workers/$1/package.json"
-sed "s/WORKER_NAME/$1/" < worker-template/wrangler.toml > "workers/$1/wrangler.toml"
+cp template-worker/index.js template-worker/build.sh "workers/$1/"
+sed "s/WORKER_NAME/$1/" < template-worker/package.json > "workers/$1/package.json"
+sed "s/WORKER_NAME/$1/" < template-worker/wrangler.toml > "workers/$1/wrangler.toml"
+code "workers/$1/index.js"
