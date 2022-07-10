d17c5664c7c57a887977201d740886be85cc5069
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-07-10 22:21:24 +1000

    Reorder build script commands

--- workers/rss-feeds/build.sh
+++ workers/rss-feeds/build.sh
@@ -3,12 +3,11 @@
 set -euo pipefail
 
 prettier --ignore-path "../../.prettierignore" --check "**/*.(js|json)"
-
-export CLOUDFLARE_ACCOUNT_ID=$(pass show workers/cloudflare-account-id)
-export CLOUDFLARE_API_TOKEN=$(pass show workers/cloudflare-api-token)
-
 mkdir -p build/handlebars
 handlebars feed.hbs -f build/feed.hbs.js
 esbuild index.js --bundle --external:fs --outfile=build/worker.js --log-level="warning"
+
+export CLOUDFLARE_ACCOUNT_ID=$(pass show workers/cloudflare-account-id)
+export CLOUDFLARE_API_TOKEN=$(pass show workers/cloudflare-api-token)
 
 wrangler publish
