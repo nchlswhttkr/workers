d4117c6548ea1a6a7b17a676e7e76cadb445ee48
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-11 19:19:08 +1000

    Use custom domains for nchlswhttkr-dot-com

--- workers/nchlswhttkr-dot-com/index.js
+++ workers/nchlswhttkr-dot-com/index.js
@@ -11,18 +11,20 @@ async function handleRequest(event) {
   }
 
   const url = new URL(event.request.url);
-  if (url.hostname.startsWith("blog")) {
-    return redirect(`https://nicholas.cloud/blog${url.pathname}`);
-  } else if (url.hostname.startsWith("resume")) {
-    return redirect(`https://nicholas.cloud/resume`);
-  } else if (url.hostname.startsWith("source")) {
-    return redirect("https://github.com/nchlswhttkr/website/");
-  } else if (url.hostname.startsWith("barhack")) {
-    return redirect("https://github.com/nchlswhttkr/barhack/");
-  } else if (url.hostname.startsWith("www")) {
-    return redirect(`https://nicholas.cloud${url.pathname}`);
-  } else if (url.hostname.startsWith("dalordish")) {
-    return redirect("https://www.youtube.com/watch?v=ktxtvMqmqu8");
+  switch (url.hostname) {
+    case "blog.nchlswhttkr.com":
+      return redirect(`https://nicholas.cloud/blog${url.pathname}`);
+    case "resume.nchlswhttkr.com":
+      return redirect(`https://nicholas.cloud/resume`);
+    case "source.nchlswhttkr.com":
+      return redirect("https://github.com/nchlswhttkr/website/");
+    case "barhack.nchlswhttkr.com":
+      return redirect("https://github.com/nchlswhttkr/barhack/");
+    case "www.nchlswhttkr.com":
+      return redirect(`https://nicholas.cloud${url.pathname}`);
+    case "dalordish.nchlswhttkr.com":
+      return redirect("https://www.youtube.com/watch?v=ktxtvMqmqu8");
+    default:
+      return redirect(`https://nicholas.cloud${url.pathname}`);
   }
-  return redirect(`https://nicholas.cloud${url.pathname}`);
 }
