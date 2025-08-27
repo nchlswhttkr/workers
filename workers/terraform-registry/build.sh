acc9e5bb3306c27a8526602e8a6cff8c532b321e
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-08-27 21:59:44 +1000

    Automate builds and deployments (#4)

--- workers/terraform-registry/build.sh
+++ workers/terraform-registry/build.sh
@@ -6,8 +6,10 @@ prettier --ignore-path "../../.prettierignore" --check .
 eslint --ignore-path "../../.eslintignore" .
 esbuild index.js --bundle --outfile="build/worker.js" --log-level="warning"
 
-VAULT_TOKEN="$(pass show vault/root-token)"
-export VAULT_TOKEN
+if [[ -z "${VAULT_TOKEN:-}" ]]; then
+    VAULT_TOKEN="$(pass show vault/root-token)"
+    export VAULT_TOKEN
+fi
 
 CLOUDFLARE_ACCOUNT_ID=$(vault kv get -field cloudflare-account-id buildkite/workers)
 export CLOUDFLARE_ACCOUNT_ID
