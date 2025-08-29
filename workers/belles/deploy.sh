5cab3d7173ae9f581555e680e60e5821a2971c65
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-08-29 22:34:00 +1000

    Fix CI check

--- workers/belles/deploy.sh
+++ workers/belles/deploy.sh
@@ -12,7 +12,7 @@ export CLOUDFLARE_ACCOUNT_ID
 CLOUDFLARE_API_TOKEN=$(vault kv get -field cloudflare-api-token buildkite/workers)
 export CLOUDFLARE_API_TOKEN
 
-if [[ "${BUILDKITE:-}" == "true" ]]; then
+if [[ -z "${BUILDKITE:-}" ]]; then
     pass show up/access-token | wrangler secret put UP_ACCESS_TOKEN
 fi
 vault kv get -field up-webhook-secret-key buildkite/workers/belles | wrangler secret put UP_WEBHOOK_SECRET_KEY
