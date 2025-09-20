6873a41511f62baf7f22ce8b701a8e717ac214ea
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-09-20 17:29:15 +1000

    Serve 418 after HTTPS redirect
    Mostly just to account for dev environment now

--- workers/enforce-https/index.ts
+++ workers/enforce-https/index.ts
@@ -1,10 +1,16 @@
 import { withTelemetry } from "@nchlswhttkr/cloudflare-workers-otel";
 
 export default withTelemetry("enforce-https", {
-  async fetch(request: Request) {
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.protocol !== "http:") {
+      return new Response("", { status: 418 });
+    }
+
+    url.protocol = "https:";
     return new Response("", {
       status: 301,
-      headers: { Location: request.url.replace("http", "https") },
+      headers: { Location: url.toString() },
     });
   },
 });
